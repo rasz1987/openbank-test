8fafbd87e997e94cf2866cf614a40dc09db8787e
rasz1987 <rodolfosifontes1@gmail.com>
2022-07-25 22:26:29 +0100

    update locales

--- src/locale/interface.tsx
+++ src/locale/interface.tsx
@@ -1,5 +1,7 @@
 export interface Locale {
   translation: {
     message: string;
+    back: string;
+    next: string;
   }
 }
---
+++ src/pages/passwordWizard/steps/Step2.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const Step2: FC = () => {
+  return (
+    <div>Step2</div>
+  )
+};
+
+export default Step2;
